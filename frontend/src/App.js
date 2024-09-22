2c4540864b0b4e71adafeab02807c9de5f98e5b0
David A. Vargas <davidavargas.dev@gmail.com>
2024-09-21 20:15:54 -0500

    home page set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import Home from "./pages/home/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-      Hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+        <h1>Tech Tutor</h1>
+    </div>
   )
 }
 

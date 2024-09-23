f998b68a1f8dd8ae62bbafc67e4e81d2c593285d
JesusMLares <twinlares2@hotmail.com>
2024-09-23 12:01:29 -0700

    Functional prisma and graphql test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 //Primsa
+import React from 'react';
+import TestGraphQL from './components/testGraphQL/testGraphQL';
 
 function App() {
   return (
     <div >
-      Hello
+      <TestGraphQL />
     </div>
   );
 }

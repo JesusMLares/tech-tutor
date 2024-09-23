7b3b873942bf5a3da6b22e2e06d6284db9a506f1
David A. Vargas <davidavargas.dev@gmail.com>
2024-09-23 13:46:37 -0500

    left main section

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Navbar from '../../components/navbar/navbar';
+import home from './home.css';
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Navbar />
-      <div className="home-content">
-        <h1>Welcome to Tech Tutor</h1>
-        <p>Find your perfect coding mentor today!</p>
+      <div className="home-container">
+        <div className="home-left">
+          <p className='home-slogan'>Learn to code and innovate, with a guide to help you elevate Tech Tutor</p>
+          <h1 className="home-title">1 on 1 Mentorship</h1>
+          <div className="search-bar">
+            <input 
+              type="text" 
+              placeholder="Search for mentors or skills..."
+              className="search-input"
+            />
+            <button className="search-button">Search</button>
+          </div>
+        </div>
+        <div className="home-right">
+          {/* for the animation for the mentors */} 
+        </div>
       </div>
     </div>
   );

9c870bab4b24a2f6d6ceb233604a5230d08af6f6
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-09-25 13:34:36 -0700

    Added correct redirects for user logins

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./login.css";
+import { useNavigate } from "react-router-dom";
+
+
 
 function Login() {
+  const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -11,6 +16,13 @@ function Login() {
     // This only returns data to the console needs to be changed in the future 
     const formJson = Object.fromEntries(formData.entries());
     console.log(formJson);
+
+    // Logic for where a user is routed to based on whether or not they check the box
+    if(formJson.checkbox === "on"){
+      navigate("/mentor/account");
+    } else {
+      navigate("/user/account");
+    }
   }
 
   return (

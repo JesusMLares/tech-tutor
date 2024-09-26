8e2236716728bd9cc5517728d866e0971f1f3bdf
JesusMLares <twinlares2@hotmail.com>
2024-09-26 15:17:28 -0700

    CreatePost working frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import TestCreateUser from './components/Test/TestUser/TestCreateUser';
 import TestUpdateUser from './components/Test/TestUser/TestUpdateUser';
+import TestCreatePost from './components/Test/TestPost/TestCreatePost';
 
 function App() {
   return (
     <div >
       <TestCreateUser />
       <TestUpdateUser />
+      <TestCreatePost />
     </div>
     
   );
--- frontend/src/components/Test/TestPost/TestCreatePost.jsx
+++ frontend/src/components/Test/TestPost/TestCreatePost.jsx
@@ -4,12 +4,70 @@ import { GraphQLClient, gql } from "graphql-request"
 const client = new GraphQLClient("http://localhost:5000")
 
 const CREATE_POST_MUTATION = gql`
-
+  mutation CreatePost($input: CreatePostInput!){
+    createPost(input: $input){
+      id
+      title
+      content
+      published
+      authorId
+    }
+  }
 `
 
 const TestCreatePost = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    published: false,
+    authorId: ""
+  })
+
+
+  const handleFormChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await client.request(CREATE_POST_MUTATION, { input: formData });
+      setFormData({ title: "", content: "", published: false, authorId: "" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>TestCreatePost</div>
+    <form onSubmit={handleSubmit}>
+      <h1>Create Post</h1>
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleFormChange}
+        placeholder="Title"
+      />
+      <input
+        type="text"
+        name="content"
+        value={formData.content}
+        onChange={handleFormChange}
+        placeholder="Content"
+      />
+      <input
+        type="text"
+        name="authorId"
+        value={formData.authorId}
+        onChange={handleFormChange}
+        placeholder="Author ID"
+      />
+      <button
+        type="submit"
+      >
+        Create Post
+      </button>
+    </form>
   )
 }
 

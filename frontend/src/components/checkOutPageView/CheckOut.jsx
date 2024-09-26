b4e9f7f9f2dd8731b50e19e556a1de72fdde1879
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-09-25 19:58:14 -0700

    Added route and files for checkout page

---
+++ frontend/src/components/checkOutPageView/CheckOut.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./checkOut.css";
+
+
+function CheckOut() {
+
+  return (
+    <div className="checkout-main-container">
+      <h1>This is the Checkout page</h1>
+    </div>
+  );
+}
+
+export default CheckOut;
---
+++ frontend/src/pages/checkoutPage/CheckOutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CheckOut from '../../components/checkOutPageView/CheckOut';
+
+function CheckOutPage() {
+  return (
+    <div className="checkout">
+      <CheckOut />
+    </div>
+  );
+}
+
+export default CheckOutPage;

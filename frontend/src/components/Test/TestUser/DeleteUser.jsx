7ff41f5facfa01c681f5528ba2e0736faa04aa51
JesusMLares <twinlares2@hotmail.com>
2024-09-30 15:28:43 -0700

    Refactored DeleteUser

--- frontend/src/components/Test/TestUser/DeleteUser.jsx
+++ frontend/src/components/Test/TestUser/DeleteUser.jsx
@@ -3,9 +3,44 @@ import { GraphQLClient, gql } from "graphql-request"
 
 const client = new GraphQLClient("http://localhost:5000")
 
+const DELETE_USER_MUTATION = gql`
+  mutation DeleteUser($id: String!) {
+    deleteUser(id: $id) {
+      id
+    }
+  }
+`
 const DeleteUser = () => {
+  const [userId, setUserId] = useState("")
+
+  const handleDeleteUser = async () => {
+    try {
+      await client.request(DELETE_USER_MUTATION, { id: userId })
+      alert("User deleted successfully")
+    } catch (error) {
+      console.error(error)
+      alert("Failed to delete user")
+    }
+  }
+
+  const handleFormChange = (e) => {
+    setUserId(e.target.value)
+  }
+
   return (
-    <div>DeleteUser</div>
+    <div>
+      <h1>Delete User</h1>
+      <form onSubmit={handleDeleteUser}>
+        <input
+          type="text"
+          name="userId"
+          value={userId}
+          onChange={handleFormChange}
+          placeholder="User ID"
+        />
+        <button type="submit">Delete User</button>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/components/Test/TestUser/QueryUsers.jsx
+++ frontend/src/components/Test/TestUser/QueryUsers.jsx
@@ -3,6 +3,30 @@ import { GraphQLClient, gql } from "graphql-request"
 
 const client = new GraphQLClient("http://localhost:5000")
 
+const GET_USERS_QUERY = gql`
+  query {
+    users {
+      id
+      firstName
+      lastName
+      email
+      role
+    }
+  }
+`
+
+const GET_USER_QUERY = gql`
+  query User($id: String!) {
+    user(id: $id) {
+      id
+      firstName
+      lastName
+      email
+      role
+    }
+  }
+`
+
 const QueryUsers = () => {
   return (
     <div>QueryUsers</div>

ead026ad6c32a3f82623d6eff38ae4f5ebabb68c
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-10-01 21:32:55 -0700

    Refactor login and mentor page views

--- frontend/src/components/accounts/accountNav/accountNav.jsx
+++ frontend/src/components/accounts/accountNav/accountNav.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './accountNav.css';
 import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function AccountNav() {
   const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   function logOut() {
     // Add additional logic for logging the user out 
@@ -31,7 +43,29 @@ function AccountNav() {
           </li>
           <li className="nav-item">
           {/* <button className="log-out-button" onClick={() => navigate("/")}>Log Out</button> */}
-          <button className="log-out-button" onClick={logOut}>Log Out</button>
+          <Button
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          >
+            Account
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button'
+            }}
+          >
+            <MenuItem onClick={() => navigate("/user/account")}>My Account</MenuItem>
+            {/* // Will have to add logic to the above function to change the route depending 
+            // on if the user is a mentor */}
+            <MenuItem onClick={logOut}>Logout</MenuItem>
+          </Menu>
           </li>
         </ul>
       </div>
--- frontend/src/components/accounts/userAccountView/UserAccount.jsx
+++ frontend/src/components/accounts/userAccountView/UserAccount.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import AccountNav from "../accountNav/accountNav";
 import "./Account.css";
 import AppointmentCard from "../loginAppointmentCards/AppointmentCard";
+import { Modal, Box, Button, Typography } from '@mui/material';
 
 function UserAccount() {
+  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  function handleDelete() {
+    console.log("Account Deleted");
+    handleClose();
+  }
   return (
     <div className="user-account-container">
       <AccountNav />
@@ -17,6 +27,9 @@ function UserAccount() {
             <p>
               <strong>Email:</strong> johndoe@example.com
             </p>
+            <button onClick={handleOpen} className="account-delete-btn">
+              Delete Account
+            </button>
           </div>
           <div className="user-appointment-container">
             <h1 className="user-account-header">Your Appointments</h1>
@@ -24,6 +37,29 @@ function UserAccount() {
           </div>
         </div>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box className="modal-box">
+          <Typography id="modal-title" variant="h6" component="h2">
+            Are you sure you would like to delete your account?
+          </Typography>
+          <Typography id="modal-description" sx={{ mt: 2 }}>
+            This action cannot be undone.
+          </Typography>
+          <div className="modal-buttons">
+            <Button variant="contained" color="error" onClick={handleDelete}>
+              Yes, delete my account
+            </Button>
+            <Button variant="outlined" onClick={handleClose}>
+              No, go back
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }

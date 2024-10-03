b83f127dea2e9b370a4514ace70e5c1b4c07069d
JesusMLares <twinlares2@hotmail.com>
2024-10-02 20:25:21 -0700

    Create user

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,32 +1,30 @@
-import React from "react";
-import "./login.css";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
-
-
+import React from "react"
+import "./login.css"
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
 
 function Login() {
-  const navigate = useNavigate();
-  const { login } = useAuth();
+  const navigate = useNavigate()
+  const { login } = useAuth()
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    const form = e.target;
-    const formData = new FormData(form);
+    const form = e.target
+    const formData = new FormData(form)
 
-    // This only returns data to the console needs to be changed in the future 
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
+    // This only returns data to the console needs to be changed in the future
+    const formJson = Object.fromEntries(formData.entries())
+    console.log(formJson)
 
-    login({ name: formJson.email });
+    login({ name: formJson.email })
 
     // Logic for where a user is routed to based on whether or not they check the box
-    if(formJson.checkbox === "on"){
-      navigate("/mentor/account");
+    if (formJson.checkbox === "on") {
+      navigate("/mentor/account")
     } else {
-      navigate("/user/account");
+      navigate("/user/account")
     }
   }
 
@@ -34,7 +32,11 @@ function Login() {
     <div className="main-container">
       <div className="side-bar">
         <div>
-          <img src='/img/tech-tutor-pc-nobg.png' alt='Tech Tutor Logo' className='login-logo' />
+          <img
+            src="/img/tech-tutor-pc-nobg.png"
+            alt="Tech Tutor Logo"
+            className="login-logo"
+          />
         </div>
       </div>
       <div className="login-section">
@@ -50,17 +52,21 @@ function Login() {
               <input className="inputs" type="password" name="password" />
             </label>
             <label htmlFor="mentor-checkbox">
-              I am a mentor 
+              I am a mentor
               <input type="checkbox" id="mentor-checkbox" name="checkbox" />
-            <Link to="/signUp" className="sign-up-link">Sign Up</Link>
+              <Link to="/signUp" className="sign-up-link">
+                Sign Up
+              </Link>
             </label>
             <br />
-            <button type="submit" className="inputs">Log in</button>
+            <button type="submit" className="inputs">
+              Log in
+            </button>
           </form>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login

444e4f8189162b6cf884e52d414daa10d82057e9
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-10-03 09:03:34 -0700

    Login page styling changes done

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react"
-import "./login.css"
-import { useNavigate } from "react-router-dom"
-import { useCurrentUser } from "../../context/CurrentUser" // Import the useCurrentUser hook
-import { GraphQLClient, gql } from "graphql-request"
-import { Link } from "react-router-dom"
-const client = new GraphQLClient("http://localhost:5000")
+import React, { useEffect, useState } from "react";
+import "./login.css";
+import { useNavigate } from "react-router-dom";
+import { useCurrentUser } from "../../context/CurrentUser"; // Import the useCurrentUser hook
+import { GraphQLClient, gql } from "graphql-request";
+import { Link } from "react-router-dom";
+const client = new GraphQLClient("http://localhost:5000");
 
 const LOGIN_MUTATION = gql`
   mutation LoginUser($email: String!, $password: String!) {
@@ -23,7 +23,7 @@ const LOGIN_MUTATION = gql`
       }
     }
   }
-`
+`;
 // if (formJson.checkbox === "on") {
 //   navigate("/mentor/account")
 // } else {
@@ -34,41 +34,41 @@ function Login() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-  })
-  const navigate = useNavigate()
-  const { currentUser, updateToken } = useCurrentUser()
+  });
+  const navigate = useNavigate();
+  const { currentUser, updateToken } = useCurrentUser();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const { email, password } = formData
+    e.preventDefault();
+    const { email, password } = formData;
     try {
       const { loginUser } = await client.request(LOGIN_MUTATION, {
         email,
         password,
-      })
-      updateToken(loginUser.token)
+      });
+      updateToken(loginUser.token);
       setFormData({
         email: "",
         password: "",
-      })
-      navigate("/")
+      });
+      navigate("/");
     } catch (error) {
-      console.error(error)
-      alert("Failed to login")
+      console.error(error);
+      alert("Failed to login");
     }
-  }
+  };
 
   const handleFormChange = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleLogout = () => {
-    updateToken(null)
-  }
+    updateToken(null);
+  };
 
   return (
     <div className="main-container">
@@ -83,9 +83,8 @@ function Login() {
       </div>
       <div className="login-section">
         <div className="login-box">
-          <h1>Login</h1>
+          <h1>Log in</h1>
           <form method="post" onSubmit={handleSubmit}>
-            <p>Email</p>
             <label>
               <input
                 className="inputs"
@@ -96,7 +95,6 @@ function Login() {
                 placeholder="Email"
               />
             </label>
-            <p>Password</p>
             <label>
               <input
                 className="inputs"
@@ -107,13 +105,18 @@ function Login() {
                 placeholder="Password"
               />
             </label>
-            <Link to="/signUp" className="sign-up-link">
-              Sign Up
-            </Link>
             <br />
-            <button type="submit" className="inputs">
+            <button type="submit" className="login-page-button">
               Log in
             </button>
+            <div className="login-links-container">
+              <Link to="/" className="login-home-link">
+                Home
+              </Link>
+              <Link to="/signUp" className="login-sign-up-link">
+                Sign Up
+              </Link>
+            </div>
           </form>
         </div>
       </div>
@@ -126,7 +129,7 @@ function Login() {
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;

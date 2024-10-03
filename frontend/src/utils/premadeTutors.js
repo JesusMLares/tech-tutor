1932ae6a40134b59a01fe0b88268da0d118c35f0
JesusMLares <twinlares2@hotmail.com>
2024-10-02 22:25:34 -0700

    Fix tutors generating on all pages, and ammount of tutors

--- frontend/src/utils/premadeTutors.js
+++ frontend/src/utils/premadeTutors.js
@@ -1,47 +1,58 @@
-import { getMultipleRandomTutors } from "./tutorGenerator"
+import { getMultipleRandomTutors } from './tutorGenerator';
 
 const generatePremadeTutors = async () => {
-  const entryLevelTutors = await getMultipleRandomTutors(16, 1, 2)
-  const juniorTutors = await getMultipleRandomTutors(16, 1, 2)
-  const seniorTutors = await getMultipleRandomTutors(16, 1, 2)
-  const onlineTutors = await getMultipleRandomTutors(8, 1, 5)
+  const entryLevelTutors = await getMultipleRandomTutors(2, 1, 2);
+  const juniorTutors = await getMultipleRandomTutors(2, 1, 2);
+  const seniorTutors = await getMultipleRandomTutors(2, 1, 2);
+  const onlineTutors = await getMultipleRandomTutors(2, 1, 5);
 
   const allTutors = [
     ...entryLevelTutors.map((tutor, index) => ({
       ...tutor,
       id: `entry-${index + 1}`,
-      level: "Entry Level",
-      rating: Math.random() + 1,
+      level: 'Entry Level',
+      rating: Math.random() + 1
     })),
     ...juniorTutors.map((tutor, index) => ({
       ...tutor,
       id: `junior-${index + 1}`,
-      level: "Junior",
-      rating: Math.random() + 2,
+      level: 'Junior',
+      rating: Math.random() + 2
     })),
     ...seniorTutors.map((tutor, index) => ({
       ...tutor,
       id: `senior-${index + 1}`,
-      level: "Senior",
-      rating: Math.random() + 3,
+      level: 'Senior',
+      rating: Math.random() + 3
     })),
     ...onlineTutors.map((tutor, index) => ({
       ...tutor,
       id: `online-${index + 1}`,
-      level: "Online",
-      rating: Math.random() * 4 + 1,
-    })),
-  ]
+      level: 'Online',
+        rating: Math.random() * 4 + 1 
+    }))
+  ];
 
-  return allTutors
-}
+  return allTutors;
+};
+
+let premadeTutorsCache = null;
+
+const getPremadeTutors = async () => {
+  if (!premadeTutorsCache) {
+    premadeTutorsCache = await generatePremadeTutors();
+  }
+  return premadeTutorsCache;
+};
 
 export const getTutorById = async (id) => {
-  const premadeTutors = await generatePremadeTutors()
-  premadeTutors.find((tutor) => tutor.id === id)
-}
+  const premadeTutors = await getPremadeTutors();
+  return premadeTutors.find(tutor => tutor.id === id);
+};
 
 export const getTutorsByLevel = async (level) => {
-  const premadeTutors = await generatePremadeTutors()
-  premadeTutors.filter((tutor) => tutor.level === level)
-}
+  const premadeTutors = await getPremadeTutors();
+  return premadeTutors.filter(tutor => tutor.level === level);
+};
+
+export { generatePremadeTutors };

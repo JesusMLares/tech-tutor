eed5f206e4d258d2931d64fb3a466ce94a232fce
JesusMLares <twinlares2@hotmail.com>
2024-10-02 18:54:33 -0700

    Bomba merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import Home from "./pages/home/home";
-import { AuthProvider } from './context/AuthContext';
-import LoginPage from "./pages/loginPage/loginPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserAccountPage from './pages/userAccountPage/userAccountPage';
-import MentorAccountPage from './pages/mentorAccountPage/MentorAccountPage';
-import SignUpPage from './pages/signUpPage/SignUpPage';
-import Contact from './pages/contact/contact';
-import About from './pages/about/about';
-import FindTutor from './pages/findTutor/findTutor';
-import TutorPage from './components/tutorpage/tutorpage';
-import PaymentPage from "./pages/payment/PaymentView";
-import PaymentSuccessPage from "./pages/paymentSuccessView/PaymentSuccessView";
-import { AppointmentProvider } from "./context/AppointmentContext";
+import React from "react"
+import Home from "./pages/home/home"
+import { AuthProvider } from "./context/AuthContext"
+import LoginPage from "./pages/loginPage/loginPage"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import UserAccountPage from "./pages/userAccountPage/userAccountPage"
+import MentorAccountPage from "./pages/mentorAccountPage/MentorAccountPage"
+import SignUpPage from "./pages/signUpPage/SignUpPage"
+import Contact from "./pages/contact/contact"
+import About from "./pages/about/about"
+import FindTutor from "./pages/findTutor/findTutor"
+import TutorPage from "./components/tutorpage/tutorpage"
+import PaymentPage from "./pages/payment/PaymentView"
+import PaymentSuccessPage from "./pages/paymentSuccessView/PaymentSuccessView"
+import { AppointmentProvider } from "./context/AppointmentContext"
 import { CurrentUserProvider } from "./context/CurrentUser"
 
 function App() {
   return (
-    <AuthProvider>
-      <AppointmentProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/user/account" element={<UserAccountPage />} />
-            <Route path="/mentor/account" element={<MentorAccountPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/tutors" element={<FindTutor />} />
-            <Route path="/tutor/:id" element={<TutorPage />} />
-            <Route path="/payment" element={<PaymentPage />} />
-            <Route path="/confirmation" element={<PaymentSuccessPage />} />
-          </Routes>
-        </Router>
-      </AppointmentProvider>
-    </AuthProvider>
-  );
+    <CurrentUserProvider>
+      <AuthProvider>
+        <AppointmentProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/user/account" element={<UserAccountPage />} />
+              <Route path="/mentor/account" element={<MentorAccountPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/tutors" element={<FindTutor />} />
+              <Route path="/tutor/:id" element={<TutorPage />} />
+              <Route path="/payment" element={<PaymentPage />} />
+              <Route path="/confirmation" element={<PaymentSuccessPage />} />
+            </Routes>
+          </Router>
+        </AppointmentProvider>
+      </AuthProvider>
+    </CurrentUserProvider>
+  )
 }
 
 export default App

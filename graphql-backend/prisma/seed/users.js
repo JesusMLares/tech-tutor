f03082f3e8c9c21685087b75d885398a0eed5515
JesusMLares <twinlares2@hotmail.com>
2024-10-04 13:43:11 -0700

    Find Tutors Page

--- graphql-backend/prisma/seed/users.js
+++ graphql-backend/prisma/seed/users.js
@@ -5,18 +5,20 @@ const prisma = new PrismaClient()
 
 async function main() {
   const users = []
-  const amount = 5
+  const amount = 10
 
-  for (let i = 0; i < amount ; i++) {
+  for (let i = 0; i < amount; i++) {
     users.push({
       firstName: faker.person.firstName(),
       lastName: faker.person.lastName(),
       email: faker.internet.email(),
       password_hash: faker.internet.password(),
-      role: faker.helpers.arrayElement(["USER", "TUTOR"]),
+      role: faker.helpers.arrayElement(["TUTOR"]),
       imageUrl: faker.image.avatar(),
-      hourlyRate: faker.number.float({ min: 10, max: 100 }),
-      rating: faker.number.float({ min: 1, max: 5 }),
+      hourlyRate: parseFloat(
+        faker.number.float({ min: 10, max: 100 }).toFixed(2)
+      ),
+      rating: parseFloat(faker.number.float({ min: 0, max: 5 }).toFixed(1)),
       skills: faker.helpers.arrayElements(
         ["JavaScript", "Python", "Java", "C#", "Ruby"],
         3

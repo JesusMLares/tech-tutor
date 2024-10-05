a26948e94db8baa0ed4aaf44f6ae6b2c65448ac8
JesusMLares <twinlares2@hotmail.com>
2024-10-05 11:50:57 -0700

    Fetch real people images

--- graphql-backend/prisma/seed/users.js
+++ graphql-backend/prisma/seed/users.js
@@ -3,18 +3,26 @@ const { faker } = require("@faker-js/faker")
 
 const prisma = new PrismaClient()
 
+async function fetchAvatar(gender) {
+  const response = await fetch(`https://xsgames.co/randomusers/avatar.php?g=${gender}`);
+  return response.url;
+}
+
 async function main() {
-  const users = []
-  const amount = 10
+  const users = [];
+  const amount = 10;
 
   for (let i = 0; i < amount; i++) {
+    const gender = faker.helpers.arrayElement(['male', 'female']);
+    const imageUrl = await fetchAvatar(gender); // Await the fetchAvatar function
+
     users.push({
-      firstName: faker.person.firstName(),
+      firstName: faker.person.firstName({ gender }),
       lastName: faker.person.lastName(),
       email: faker.internet.email(),
       password_hash: faker.internet.password(),
       role: faker.helpers.arrayElement(["TUTOR"]),
-      imageUrl: faker.image.avatar(),
+      imageUrl, // Use the fetched image URL
       hourlyRate: parseFloat(
         faker.number.float({ min: 10, max: 100 }).toFixed(2)
       ),
@@ -30,14 +38,15 @@ async function main() {
         3
       ),
       isAvailable: true,
-    })
+    });
+    console.log(users[i]);
   }
 
   await prisma.user.createMany({
     data: users,
-  })
+  });
 
-  console.log(`${amount} users created`)
+  console.log(`${amount} users created`);
 }
 
 main()

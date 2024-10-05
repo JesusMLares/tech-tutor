16f8c51a6e4f9d852a5a5cd9d4da7338c445c6b7
JesusMLares <twinlares2@hotmail.com>
2024-10-05 09:31:19 -0700

    cors blocking

--- stripe-backend/index.js
+++ stripe-backend/index.js
@@ -1,18 +1,24 @@
 // Import and initializes express
 const express = require("express");
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT;
+
+// Passes the route to each specific file, therefore 
+// routes do not have to be explicity defined in their file
+app.use(cors({
+    origin: 'https://tech-tutor.onrender.com', // Replace with your frontend domain
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+app.use(express.json())
+app.use('/checkOut', checkOut)
+app.use('/products', products)
 
 // Imports the routes from other files uisng express router
 const checkOut = require('./routes/checkOut')
 const products = require('./routes/products')
-
-// Passes the route to each specific file, therefore 
-// routes do not have to be explicity defined in their file
-app.use(express.json())
-app.use('/checkOut', checkOut)
-app.use('/products', products)
 
 app.listen(PORT, () => {
     console.log("The server is working if you can read this")
